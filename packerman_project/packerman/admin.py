b965ae05d09f40c250894bcacccb6ccc3755ccd0
Rhys Frazier <rhysrfrazier@gmail.com>
2023-11-29 17:36:09 -0500

    broke it, fixed it, going to stop touching it tonight

--- packerman_project/packerman/admin.py
+++ packerman_project/packerman/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Item, Event, User_Item, Event_Item
+
+admin.site.register(User)
+admin.site.register(Item)
+admin.site.register(Event)
+admin.site.register(User_Item)
+admin.site.register(Event_Item)
--- packerman_project/packerman/models.py
+++ packerman_project/packerman/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 import uuid
 
-class Users(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=100)
     img = models.TextField()
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
-        return self.user_id
+        return self.name
 
-class Items(models.Model):
+class Item(models.Model):
     desc = models.TextField()
     category = models.CharField(max_length=200)
     sub_category = models.CharField(max_length=200)
@@ -20,9 +20,9 @@ class Items(models.Model):
     item_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
-        return self.item_id
+        return self.desc
     
-class Events(models.Model):
+class Event(models.Model):
     name = models.CharField(max_length=200)
     start_date = models.DateField(auto_now=False, auto_now_add=False)
     end_date = models.DateField(auto_now=False, auto_now_add=False)
@@ -31,16 +31,16 @@ class Events(models.Model):
     def __str__(self):
         return self.name
     
-class User_Items(models.Model):
-    item_id = models.ForeignKey(Items, on_delete=models.CASCADE, related_name='items')
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='users')
+class User_Item(models.Model):
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='items')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
     out_date = models.DateField(auto_now=False, auto_now_add=False)
     in_date = models.DateField(auto_now=False, auto_now_add=False)
 
-class Event_Items(models.Model):
-    item_id = models.ForeignKey(Items, on_delete=models.CASCADE, related_name='packed_items')
-    event_id = models.ForeignKey(Events, on_delete=models.CASCADE, related_name = 'events')
+class Event_Item(models.Model):
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='packed_items')
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE, related_name = 'events')
     packed_date = models.DateField(auto_now=False, auto_now_add=False)
     unpacked_date = models.DateField(auto_now=False, auto_now_add=False)
-    packed_by = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='packing_user')
-    unpacked_by = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='unpacking_user')
+    packed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='packing_user')
+    unpacked_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='unpacking_user')
