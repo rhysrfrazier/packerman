e100e901c3fa9f59531adb33f17d5a11214177ff
Rhys Frazier <rhysrfrazier@gmail.com>
2023-11-29 12:04:53 -0500

    user model - seems to not be broken

--- packerman_project/packerman/models.py
+++ packerman_project/packerman/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+import uuid
 
-# class User(models.Model):
-#     name = models.CharField(max_length=100)
-#     icon = models.Textfield()
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.TextField()
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
+    def __str__(self):
+        return self.user_id
