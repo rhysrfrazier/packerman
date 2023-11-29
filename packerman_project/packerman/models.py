7cb7712dfe0f5577725f5cacb03778837bcb21e6
Rhys Frazier <rhysrfrazier@gmail.com>
2023-11-29 12:43:49 -0500

    second model successfully added

--- packerman_project/packerman/models.py
+++ packerman_project/packerman/models.py
@@ -3,8 +3,25 @@ import uuid
 
 class User(models.Model):
     name = models.CharField(max_length=100)
-    icon = models.TextField()
+    img = models.TextField()
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return self.user_id
+
+class Items(models.Model):
+    desc = models.TextField()
+    category = models.CharField(max_length=200)
+    sub_category = models.CharField(max_length=200)
+    img = models.TextField()
+    instructions = models.TextField()
+    needs_repair = models.BooleanField(default=False)
+    lost = models.BooleanField(default=False)
+    item_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return self.item_id
+    
+# class Events(models.Model):
+#     name = models.CharField(max_length=200)
+#     start_date = models.DateField(auto_now=False, auto_now_add=False)
